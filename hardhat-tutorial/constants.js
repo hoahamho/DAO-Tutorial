3c1cc32621ea7e449ea75c0ec002e463387b284f
hoahamho <hoaciven@gmail.com>
2022-12-07 15:12:10 +0700

    some unknown error

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0xB916c28D99820c42291554197bb5107a1bDDD105";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
